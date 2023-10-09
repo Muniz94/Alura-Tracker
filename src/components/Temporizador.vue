26a6ea60cb05772e9ad8f4f8862e3960de7f09a7
muniz94 <muniz91@hotmail.com>
2023-10-09 14:41:17 -0300

    Caso esteja rodando(desabilita o play); Caso parado(desabilita o stop)

--- src/components/Temporizador.vue
+++ src/components/Temporizador.vue
@@ -2,13 +2,13 @@
   <div class="is-flex is-align-items-center is-justify-content-space-between">
 
     <Cronometro :tempoEmsegundos="tempoEmsegundos" />
-    <button class="button" @click="iniciar">
+    <button class="button" @click="iniciar" :disabled="cronometroRodando">
       <span class="icon">
         <i class="fas fa-play"></i>
       </span>
       <span>play</span>
     </button>
-    <button class="button" @click="finalizar">
+    <button class="button" @click="finalizar" :disabled="!cronometroRodando"> <!-- o dois pontos linka o estado do objeto com o atributo disabled -->
       <span class="icon">
         <i class="fas fa-stop"></i>
       </span>
@@ -30,18 +30,21 @@
     data () { // esse método define um estado inicial para o objeto
       return {
         tempoEmsegundos: 0,
-        cronometro: 0
+        cronometro: 0,
+        cronometroRodando: false
       }
     },
     methods: {
       iniciar () {
         // começar a contagem
         // 1 seg = 1000 ms
+        this.cronometroRodando = true
         this.cronometro = setInterval(() => {
           this.tempoEmsegundos += 1
         }, 1000)
       },
       finalizar () {
+        this.cronometroRodando = false
         clearInterval(this.cronometro);
       }
     }
