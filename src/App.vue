8b89da84adb10a6ac7016f9220fbf6fc2f1e3d4d
muniz94 <muniz91@hotmail.com>
2023-10-11 17:16:48 -0300

    Adicionei as linhas de código para trocar o tema

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
-  <main class="columns is-gapless is-multiline modo-escuro">
+  <main class="columns is-gapless is-multiline" :class="{ 'modo-escuro': modoEscuroAtivo }"> <!-- Se o modo escuro está ativo a classe é adicionada, caso contrário, não.-->
     <div class="column is-one-quarter">
-      <BarraLateral />
+      <BarraLateral @aoTemaAlterado="trocarTema"/>
     </div>
     <div class="column is-three-quarter conteudo">
       <Formulario @aoSalvarTarefa="salvarTarefa"/>
@@ -34,7 +34,8 @@ export default defineComponent({
     },
     data () {
       return {
-        tarefas: [] as ITarefa[]
+        tarefas: [] as ITarefa[],
+        modoEscuroAtivo: false 
       }
     },
     computed: {
@@ -45,8 +46,11 @@ export default defineComponent({
     methods: {
       salvarTarefa (tarefa: ITarefa) {
         this.tarefas.push(tarefa)
-      }
+      },
+      trocarTema (modoEscuroAtivo: boolean) {
+        this.modoEscuroAtivo = modoEscuroAtivo
     }
+  }
 });
 </script>
 
