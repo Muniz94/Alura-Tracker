8da588e1f402b56538dfdf8a615b3afdff5b9d03
muniz94 <muniz91@hotmail.com>
2023-10-11 13:30:37 -0300

    Criei o componente 'box.vue' para refatorar o código

---
+++ src/components/Box.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="box has-text-weight-bold">
+    <slot></slot> <!--Renderiza o conteúdo do 'box'(nesse caso) para dentro dessa tag-->
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'Box'
+})
+</script>
+
+<style scoped>
+.box {
+  background: #FAF0CA;
+}
+</style>
--- src/components/Tarefa.vue
+++ src/components/Tarefa.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="box has-text-weight-bold">
+  <Box>
     <div class="columns">
       <div class="column is-7">
         {{tarefa.descricao || 'Tarefa sem descrição'}}
@@ -8,18 +8,20 @@
         <Cronometro :tempoEmSegundos="tarefa.duracaoEmSegundos"/>
       </div>
     </div>
-  </div>
+  </Box>
 </template>
 
 <script lang="ts">
 import { defineComponent, PropType } from "vue";
 import Cronometro from "./Cronometro.vue";
 import ITarefa from "../interfaces/ITarefa";
+import Box from "./Box.vue"
 
 export default defineComponent({
   name: "Tarefa",
   components: {
-    Cronometro
+    Cronometro,
+    Box
   },
   props: {
     tarefa: {
@@ -30,9 +32,3 @@ export default defineComponent({
 });
 
 </script>
-
-<style scoped>
-.box {
-  background: #FAF0CA;
-}
-</style>
